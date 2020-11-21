6095a6e18fd37d77f2b98576ebee11c2fa0bfb7d
Andrew Horn <arhorn@smcm.edu>
2020-11-21 17:36:00 -0600

    added comma

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hey people, I'm Jeremy</h1>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/T1zEBh5HLH8?start=769" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allow="fullscreen"></iframe>
-    <p>I love snowboarding, and I want you to love it too. Join me and we'll make magic together.</p>
+    <p>I love snowboarding, and I want you to love it too. Join me, and we'll make magic together.</p>
   </Layout>
 )
 
