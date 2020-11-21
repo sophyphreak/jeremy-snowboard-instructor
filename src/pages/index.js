399de4b8a0c25f4f8eb61e9fd99ecf48567d3844
Andrew Horn <arhorn@smcm.edu>
2020-11-21 17:29:14 -0600

    changed hi to hey

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people, I'm Jeremy</h1>
+    <h1>Hey people, I'm Jeremy</h1>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/T1zEBh5HLH8?start=769" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <p>I love snowboarding, and I want you to love it too. Join me and we'll make magic together.</p>
   </Layout>
